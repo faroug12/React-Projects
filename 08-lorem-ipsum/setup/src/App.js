291eddf357d6094080286d04f5c4d13cc0aefdc2
faroug12 <faroug14@gmail.com>
2022-04-05 00:03:50 +0100

    finished few more projects

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,7 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
-    )
+  return <h2>lorem ipsum project setu</h2>;
 }
 
 export default App;
